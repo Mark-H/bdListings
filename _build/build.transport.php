5e4531fe41fbb418e6064aa2bbdd392b6628c17e
Mark Hamstra <hello@markhamstra.com>
2011-11-01 01:28:46 +0100

    Version bump

--- _build/build.transport.php
+++ _build/build.transport.php
@@ -22,8 +22,8 @@ set_time_limit(0);
 /* define package */
 define('PKG_NAME','bdListings');
 define('PKG_NAME_LOWER',strtolower(PKG_NAME));
-define('PKG_VERSION','0.9.2');
-define('PKG_RELEASE','dev2');
+define('PKG_VERSION','0.9.3');
+define('PKG_RELEASE','dev1');
 
 $root = dirname(dirname(__FILE__)).'/';
 $sources= array (
