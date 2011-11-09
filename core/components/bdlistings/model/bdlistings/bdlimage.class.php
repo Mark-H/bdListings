a0a2f029c4a0fd7d0d32378d5f2ab19d067805e2
Mark Hamstra <hello@markhamstra.com>
2011-11-09 02:44:10 +0100

    Abstract out file handling

--- core/components/bdlistings/model/bdlistings/bdlimage.class.php
+++ core/components/bdlistings/model/bdlistings/bdlimage.class.php
@@ -56,5 +56,40 @@ class bdlImage extends xPDOSimpleObject {
         }
         return parent::remove($ancestors);
     }
+
+    public function handleUpload (array $file = array(), $listing = 0) {
+        /* New file upload */
+        $uploadPath = $this->xpdo->getOption('bdlistings.uploadpath');
+        if (empty($uploadPath)) $uploadPath = $this->xpdo->getOption('bdlistings.assets_path',null,$this->xpdo->getOption('assets_path')).'uploads/';
+
+        $this->xpdo->getService('fileHandler','modFileHandler');
+
+        $uploadDirObj = $this->xpdo->fileHandler->make($uploadPath);
+        if (!$uploadDirObj->isWritable()) {
+            return array('error' => 'Main upload dir '.$uploadPath.' is not writable.');
+        }
+
+        /* Object Directory */
+        $objectDirPath = $uploadDirObj->getPath().$listing;
+        $objectDirObj = $this->xpdo->fileHandler->make($objectDirPath,array(),'modDirectory');
+        if (!$objectDirObj->exists()) {
+            if ($objectDirObj->create() !== true)
+                return array('error' => 'Error creating object directory.');
+        }
+        if (!$objectDirObj->isWritable()) {
+            return array('error' => 'Object Directory is not writable.');
+        }
+
+        $rand = rand(0,99999);
+        $fileDir = $objectDirObj->getPath().'/'.$rand.'-'.$file['name'];
+        $fileUrl = $listing.'/'.$rand.'-'.$file['name'];
+
+        if (move_uploaded_file($file['tmp_name'],$fileDir)) {
+            return $fileUrl;
+        } else {
+            $this->xpdo->log(modX::LOG_LEVEL_ERROR,'Error uploading file '.$file['tmp_name'].' to '.$fileDir);
+            return array('error' => 'Error uploading file.');
+        }
+    }
 }
 ?>
