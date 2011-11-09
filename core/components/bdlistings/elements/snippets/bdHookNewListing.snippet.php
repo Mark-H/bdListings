078a2e06faaf87f8a60b1d8070d0c1a0da9f2059
Mark Hamstra <hello@markhamstra.com>
2011-11-09 02:20:30 +0100

    If a chosen category is actually a subcategory and no sub was posted, set them properly.

--- core/components/bdlistings/elements/snippets/bdHookNewListing.snippet.php
+++ core/components/bdlistings/elements/snippets/bdHookNewListing.snippet.php
@@ -10,6 +10,7 @@ $listing = $modx->newObject('bdlListing');
 $listing->set('createdon',date('Y-m-d H:i:s',time()));
 
 $d = $hook->getValues();
+
 if ((int)$d['duration'] > 0) {
     $duration = (int)$d['duration'];
     $expires = time() + $duration;
@@ -19,6 +20,19 @@ if (is_numeric($d['publisheduntil']))
     $d['publisheduntil'] = date('Y-m-d H:i:s',$d['publisheduntil']);
 else if (!empty($d['publisheduntil']))
     $d['publisheduntil'] = date('Y-m-d H:i:s',strtotime($d['publisheduntil']));
+
+/* If the chosen category is a subcategory, fix the references. */
+if (empty($d['subcategory']) && !empty($d['category'])) {
+    $cat = $modx->getObject('bdlCategory',$d['category']);
+    if ($cat instanceof bdlCategory) {
+        if ($cat->get('parent') > 0) {
+            $d['category'] = $cat->get('parent');
+            $d['subcategory'] = $cat->get('id');
+        }
+    } else {
+        $d['category'] = 0;
+    }
+}
 
 $d['active'] = false;
 $d['featured'] = false;
