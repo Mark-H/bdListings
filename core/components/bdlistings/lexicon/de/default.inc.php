c7e176668b0316a6cdf5d2675be49f1008bd5341
Mark Hamstra <hello@markhamstra.com>
2011-11-25 22:37:05 +0100

    Add German translation

---
+++ core/components/bdlistings/lexicon/de/default.inc.php
@@ -0,0 +1,197 @@
+<?php
+
+
+
+$_lang['bdlistings'] = 'Anzeigen';
+
+$_lang['bdlistings.desc'] = 'Ihre Anzeige-Manager';
+
+
+
+$_lang['bdlistings.listing'] = 'Listing';
+
+$_lang['bdlistings.listings'] = 'Listings';
+
+$_lang['bdlistings.click'] = 'Click';
+
+$_lang['bdlistings.clicks'] = 'Clicks';
+
+$_lang['bdlistings.structure'] = 'Structure';
+
+$_lang['bdlistings.admin'] = 'Administration';
+
+
+
+/* Buttons / Actions / Filters */
+
+$_lang['bdlistings.create'] = 'Create [[+what]]';
+
+$_lang['bdlistings.create_here'] = 'Create [[+what]] Here';
+
+$_lang['bdlistings.update'] = 'Update [[+what]]';
+
+$_lang['bdlistings.remove'] = 'Remove [[+what]]';
+
+$_lang['bdlistings.clearfilter'] = 'Clear Filter';
+
+$_lang['bdlistings.filter_on'] = 'Filter on [[+what]]';
+
+$_lang['bdlistings.search...'] = 'Search...';
+
+$_lang['bdlistings.listing.details'] = 'Listing Details';
+
+$_lang['bdlistings.listing.location'] = 'Location &amp; Company';
+
+$_lang['bdlistings.listing.categorize'] = 'Categorization';
+
+$_lang['bdlistings.listing.images'] = 'Images';
+
+$_lang['bdlistings.listing.images.saverequired'] = 'You need to save the new listing before you can add images to it.';
+
+$_lang['bdlistings.date.start'] = 'Date From';
+
+$_lang['bdlistings.date.end'] = 'Date End';
+
+$_lang['bdlistings.date.steps'] = '# of Steps';
+
+$_lang['bdlistings.remove.confirm'] = 'Remove [[+what]]?';
+
+$_lang['bdlistings.remove.confirm.text'] = 'Are you sure you want to remove this [[+what]]? This can not be undone.';
+
+$_lang['bdlistings.calclatlong'] = 'Find Lat/Long';
+
+$_lang['bdlistings.calclatlong.desc'] = 'When enabled, this will make a request to Google Maps to retrieve the best latitude/longitude match based on the details you specified above. You will at least need the address or zip, as well as the city.';
+
+
+
+/* Error messages */
+
+$_lang['bdlistings.noresults'] = '<p>Leider keine Ergebnise gefunden! :(</p>';
+
+$_lang['bdlistings.error.noresults'] = 'No results found matching your criteria.';
+
+$_lang['bdlistings.error.object_nf'] = 'Object not found.';
+
+$_lang['bdlistings.error.save'] = 'Error saving object.';
+
+$_lang['bdlistings.error.remove'] = 'Error removing object.';
+
+$_lang['bdlistings.error.categoryinvalid'] = 'The category is invalid.';
+
+$_lang['bdlistings.error.pricegroupinvalid'] = 'The price group is invalid.';
+
+$_lang['bdlistings.error.targetinvalid'] = 'The target group is invalid.';
+
+$_lang['bdlistings.error.invalid'] = 'Invalid request.';
+
+$_lang['bdlistings.error.sortinvalid'] = 'Requested sort is not valid: [[+errors]]';
+
+$_lang['bdlistings.error.toodeep'] = 'Category [[+id]] is nested too deep, only two levels is allowed.';
+
+$_lang['bdlistings.error.fileupload'] = 'Uploading [[+file]] failed.';
+
+$_lang['bdlistings.error.filetoobig'] = 'File too big.';
+
+$_lang['bdlistings.error.invalidext'] = 'Invalid file type: [[+ext]].';
+
+$_lang['bdlistings.error.file.1'] = 'File too big: server does not allow uploads of this size.';
+
+$_lang['bdlistings.error.file.2'] = 'File too big: exceeds specified max size.';
+
+$_lang['bdlistings.error.file.3'] = 'Only part of the file was uploaded.';
+
+$_lang['bdlistings.error.file.4'] = 'No file was uploaded.';
+
+$_lang['bdlistings.error.file.6'] = 'Server misconfiguration.';
+
+$_lang['bdlistings.error.file.7'] = 'Failed to write to disk.';
+
+$_lang['bdlistings.error.file.8'] = 'Server misconfiguration.';
+
+
+
+/* Fields, somewhat alphabetical */
+
+$_lang['bdlistings.active'] = 'Active';
+
+$_lang['bdlistings.address'] = 'Address';
+
+$_lang['bdlistings.caption'] = 'Caption';
+
+$_lang['bdlistings.category'] = 'Category';
+
+$_lang['bdlistings.categories'] = 'Categories';
+
+$_lang['bdlistings.city'] = 'City';
+
+$_lang['bdlistings.clicktime'] = 'Time Clicked';
+
+$_lang['bdlistings.companyname'] = 'Company';
+
+$_lang['bdlistings.contactinfo'] = 'Contact Info';
+
+$_lang['bdlistings.country'] = 'Country';
+
+$_lang['bdlistings.createdon'] = 'Created On';
+
+$_lang['bdlistings.description'] = 'Description';
+
+$_lang['bdlistings.display'] = 'Display as';
+
+$_lang['bdlistings.email'] = 'Email';
+
+$_lang['bdlistings.ipaddress'] = 'IP Address';
+
+$_lang['bdlistings.image'] = 'Image';
+
+$_lang['bdlistings.image.current'] = 'Current Image';
+
+$_lang['bdlistings.images'] = 'Images';
+
+$_lang['bdlistings.featured'] = 'Featured';
+
+$_lang['bdlistings.keywords'] = 'Keywords';
+
+$_lang['bdlistings.latitude'] = 'Latitude';
+
+$_lang['bdlistings.longitude'] = 'Longitude';
+
+$_lang['bdlistings.name'] = 'Name';
+
+$_lang['bdlistings.neighborhood'] = 'Neighborhood';
+
+$_lang['bdlistings.sortorder'] = 'Sort Order';
+
+$_lang['bdlistings.parent'] = 'Parent';
+
+$_lang['bdlistings.phone'] = 'Phone';
+
+$_lang['bdlistings.price'] = 'Price';
+
+$_lang['bdlistings.pricegroup'] = 'Price Group';
+
+$_lang['bdlistings.pricegroups'] = 'Price Groups';
+
+$_lang['bdlistings.primary'] = 'Primary';
+
+$_lang['bdlistings.publisheduntil'] = 'Published Until';
+
+$_lang['bdlistings.referrer'] = 'Referring URL';
+
+$_lang['bdlistings.subcategory'] = 'Sub-Category';
+
+$_lang['bdlistings.target'] = 'Target Group';
+
+$_lang['bdlistings.targets'] = 'Target Groups';
+
+$_lang['bdlistings.title'] = 'Title';
+
+$_lang['bdlistings.website'] = 'Website';
+
+$_lang['bdlistings.zip'] = 'Zip';
+
+
+
+
+
+?>
